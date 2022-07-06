5d4161443a9e3d078c9b9b3cc9454492fff08c6e
esteban90-dev <stephencallard@gmail.com>
2022-07-06 10:37:17 -0600

    add static InformationCard Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
+import InformationCard from './components/InformationCard';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        <main>
+          <InformationCard />
+        </main>
+      </>
     );
   }
 }
---
+++ src/components/InformationCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class InformationCard extends React.Component {
+  render() {
+    return (
+      <section className="information">
+        <h2 className="information__title">General Information</h2>
+        <hr></hr>
+      </section>
+    );
+  }
+}
