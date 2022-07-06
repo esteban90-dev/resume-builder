b345bcb1206d594660f07446177d7c47fa866c58
esteban90-dev <stephencallard@gmail.com>
2022-07-06 10:26:28 -0600

    add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1 className="header__title">Resume Builder</h1>
+      </header>
+    );
+  }
+}
