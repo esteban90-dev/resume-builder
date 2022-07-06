e957a4237b1d6f947c25823a664361e4029df86d
esteban90-dev <stephencallard@gmail.com>
2022-07-06 14:13:26 -0600

    add static EducationCard component

---
+++ src/components/EducationCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class EducationCard extends React.Component {
+  render() {
+    return (
+      <section className="education">
+        <h2 className="education__title">Education</h2>
+        <hr></hr>
+      </section>
+    );
+  }
+}
